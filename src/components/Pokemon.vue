62d9691c257495764fee9a266f120ee468e1811e
JoseGuilhermeCA <gui.jgca@gmail.com>
2022-12-18 21:02:52 -0300

    Commit 18/12:
    Css style pokemon card (Done)

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,8 +1,10 @@
 <template>
 
-    <div class="teste">
+    <div class="container">
         <PokemonCard v-for="(pokemon) in pkm" :name="pokemon.name" :url="pokemon.url" v-bind:key="pokemon.name" />
     </div>
+
+
 
 
 </template>
@@ -11,38 +13,37 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import api from '@/services/api';
 import PokemonCard from './PokemonCard.vue';
-
 export default defineComponent({
     name: "Pokemon-vue",
-
     components: {
         PokemonCard
     },
-
     beforeMount() {
-        api.get("/pokemon?limit=181")
+        api.get("/pokemon?limit=18")
             .then(response => {
                 console.log(response)
                 response.data.results.map((e: any) => {
                     this.pkm.push(e)
-
                 })
             });
     },
-
     data() {
-
         return { pkm: [] as any }
     },
-
     methods: {
-
     },
 })
-
-
 </script>
 
 <style lang="scss">
+.container {
 
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+   
+    
+    
+
+
+}
 </style>
